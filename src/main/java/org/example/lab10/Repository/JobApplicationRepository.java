0997528c779dfcfca421ff460f697605cd64acc0
Sara_Alqahtani <159685978+SaraQ2030@users.noreply.github.com>
2024-03-25 20:45:54 +0300

    Delete src directory

--- src/main/java/org/example/lab10/Repository/JobApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.lab10.Repository;
-
-import org.example.lab10.Model.JobApp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobApplicationRepository extends JpaRepository<JobApp,Integer> {
-}
--- src/main/java/org/example/lab10/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.lab10.Repository;
-
-import org.example.lab10.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
